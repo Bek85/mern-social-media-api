8b82c536a889df07681af148a53450ee889bf25e
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-05 16:15:32 +0500

    handle basic login flow by checking existence of email and password validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,19 +1,48 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
-// Register flow
+//! REGISTER FLOW
 
 router.post('/register', async (req, res) => {
-  const newUser = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    // hash received password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // create a new user
+    const newUser = await new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // save created user to backend and return response
     const user = newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+//! LOGIN FLOW
+
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({
+      email: req.body.email,
+    });
+    !user && res.status(404).send('User not found');
+
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !validPassword && res.status(400).json('Wrong password');
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
