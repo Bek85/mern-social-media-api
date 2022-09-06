8d005b591cf77abd538344b1e3259f7c15186949
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-06 16:21:14 +0500

    handle a flow to get all posts of a user and his followers

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 //! GET A POST
 router.get('/:id', async (req, res) => {
@@ -12,6 +13,20 @@ router.get('/:id', async (req, res) => {
 });
 
 //! GET ALL POSTS
+router.get('/timeline/all', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.following.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    res.json(userPosts.concat(...friendPosts));
+  } catch (error) {
+    res.status(500).json('Something went wrong on the server');
+  }
+});
 
 //! CREATE A POST
 router.post('/', async (req, res) => {
