9f576497b3af7a78ac418ae2e14675e88c211d3a
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-06 14:48:21 +0500

    handle deleting a post

--- routes/posts.js
+++ routes/posts.js
@@ -37,4 +37,18 @@ router.put('/:id', async (req, res) => {
 
 //! DELETE A POST
 
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json('The post has been deleted');
+    } else {
+      res.status(403).json('You can delete only your post');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
