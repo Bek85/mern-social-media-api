0837c5a6394cd9a155426b6f3d997192488f69c4
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-06 13:17:10 +0500

    handle post creation flow

--- routes/posts.js
+++ routes/posts.js
@@ -19,6 +19,20 @@ router.post('/', async (req, res) => {
 
 //! UPDATE A POST
 
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json('The post has been updated');
+    } else {
+      res.status(403).json('You can update only your post');
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 //! LIKE A POST
 
 //! DELETE A POST
