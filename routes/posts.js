ba251e76fae63bd90fd35ec45cf8fdbdf27844c9
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-13 22:10:59 +0500

    fixex

--- routes/posts.js
+++ routes/posts.js
@@ -13,16 +13,16 @@ router.get('/:id', async (req, res) => {
 });
 
 //! GET ALL POSTS
-router.get('/timeline/all', async (req, res) => {
+router.get('/timeline/:userId', async (req, res) => {
   try {
-    const currentUser = await User.findById(req.body.userId);
+    const currentUser = await User.findById(req.params.userId);
     const userPosts = await Post.find({ userId: currentUser._id });
     const friendPosts = await Promise.all(
       currentUser.following.map((friendId) => {
         return Post.find({ userId: friendId });
       })
     );
-    res.json(userPosts.concat(...friendPosts));
+    res.status(200).json(userPosts.concat(...friendPosts));
   } catch (error) {
     res.status(500).json('Something went wrong on the server');
   }
