7e78b7991e558ec9078dc2a05dc995dbf2526916
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-13 17:58:54 +0500

    port update and package.json script command

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const PORT = process.env.PORT || 4000;
 
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
@@ -25,6 +26,6 @@ app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
 
-app.listen(8800, () => {
-  console.log('Server is running on port 8800');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
